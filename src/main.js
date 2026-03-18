7ee0b39c9671df43a105a4333ae8d682db9c2fc6
Dirtroad Organizing <jordan@dirtroadorganizing.org>
2026-03-17 21:54:09 -0400

    feature: Render full-screen Leaflet map with OpenStreetMap tiles

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 import './style.css'
+
+const map = L.map('map').setView([39.8, -98.5], 4)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map)
